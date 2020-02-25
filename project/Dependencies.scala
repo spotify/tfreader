0a6cf0b3781b0eb65792849ca63c17322c0f3a8a
Scala Steward <me@scala-steward.org>
2020-02-25 15:21:01 +0100

    Update cats-core to 2.1.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val protobuf = "com.google.protobuf" % "protobuf-java-util" % "3.11.1"
   lazy val tensorFlowProto = "org.tensorflow" % "proto" % "1.15.0"
   lazy val guava = "com.google.guava" % "guava" % "28.2-jre"
-  lazy val catsCore = "org.typelevel" %% "cats-core" % "2.1.0"
+  lazy val catsCore = "org.typelevel" %% "cats-core" % "2.1.1"
   lazy val fs2Io = "co.fs2" %% "fs2-io" % "2.2.2"
   lazy val caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.0-M10"
   lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full
