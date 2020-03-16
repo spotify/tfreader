2f7dba20a20d4a846b19335268c2f911b001fa1b
Scala Steward <me@scala-steward.org>
2020-03-16 14:48:04 +0100

    Update case-app to 2.0.0-M15

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   lazy val guava = "com.google.guava" % "guava" % "28.2-jre"
   lazy val catsCore = "org.typelevel" %% "cats-core" % "2.1.1"
   lazy val fs2Io = "co.fs2" %% "fs2-io" % "2.2.2"
-  lazy val caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.0-M13"
+  lazy val caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.0-M15"
   lazy val kindProjector =
     "org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full
   lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "1.105.2"
