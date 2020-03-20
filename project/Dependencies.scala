c96c6ec273949287dd02dcfea5e21a103bed9553
Scala Steward <me@scala-steward.org>
2020-03-20 20:34:00 +0100

    Update fs2-io to 2.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val tensorFlowProto = "org.tensorflow" % "proto" % "1.15.0"
   lazy val guava = "com.google.guava" % "guava" % "28.2-jre"
   lazy val catsCore = "org.typelevel" %% "cats-core" % "2.1.1"
-  lazy val fs2Io = "co.fs2" %% "fs2-io" % "2.2.2"
+  lazy val fs2Io = "co.fs2" %% "fs2-io" % "2.3.0"
   lazy val caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.0-M16"
   lazy val kindProjector =
     "org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full
