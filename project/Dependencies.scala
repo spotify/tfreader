acece6651154237adc82ff198c7a43dfb688b8c6
Scala Steward <me@scala-steward.org>
2020-07-02 03:58:50 +0200

    Update google-cloud-storage to 1.111.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   lazy val caseApp = "com.github.alexarchambault" %% "case-app" % "2.0.1"
   lazy val kindProjector =
     "org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full
-  lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "1.110.0"
+  lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "1.111.1"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.13.0"
   lazy val munit = "org.scalameta" %% "munit" % "0.7.9"
   lazy val circeParser = "io.circe" %% "circe-parser" % "0.13.0"
