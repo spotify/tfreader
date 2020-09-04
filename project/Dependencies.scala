6d75673e83da81b39f1349362aa0262b4f71c8d7
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-09-05 01:46:16 +0200

    Update google-cloud-storage to 1.113.0 (#103)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val catsCore = "org.typelevel" %% "cats-core" % "2.2.0"
   lazy val fs2Io = "co.fs2" %% "fs2-io" % "2.4.4"
   lazy val scallop = "org.rogach" %% "scallop" % "3.5.1"
-  lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "1.112.0"
+  lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "1.113.0"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.13.0"
   lazy val munit = "org.scalameta" %% "munit" % "0.7.12"
   lazy val circeParser = "io.circe" %% "circe-parser" % "0.13.0"
