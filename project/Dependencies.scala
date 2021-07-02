6ea1ef182700d9bddaf2d42dcd667e4d469e496e
Scala Steward <me@scala-steward.org>
2021-07-02 18:50:27 +0200

    Update munit to 0.7.27

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,6 +10,6 @@ object Dependencies {
   lazy val scallop = "org.rogach" %% "scallop" % "4.0.3"
   lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "1.117.1"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.14.1"
-  lazy val munit = "org.scalameta" %% "munit" % "0.7.26"
+  lazy val munit = "org.scalameta" %% "munit" % "0.7.27"
   lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.1"
 }
