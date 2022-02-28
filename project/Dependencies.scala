ef4c43eb1f3ab8aef7373e494bcac9773f4a981e
Scala Steward <me@scala-steward.org>
2022-03-01 00:45:32 +0100

    Update guava to 31.1-jre

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   lazy val protobufVersion = "3.19.4"
   lazy val protobuf =
     "com.google.protobuf" % "protobuf-java-util" % protobufVersion
-  lazy val guava = "com.google.guava" % "guava" % "31.0.1-jre"
+  lazy val guava = "com.google.guava" % "guava" % "31.1-jre"
   lazy val catsCore = "org.typelevel" %% "cats-core" % "2.7.0"
   lazy val fs2Io = "co.fs2" %% "fs2-io" % "3.2.5"
   lazy val scallop = "org.rogach" %% "scallop" % "4.1.0"
