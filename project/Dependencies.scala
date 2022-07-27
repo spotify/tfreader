2a09a3fd55671b21f12fa1248518c1a97b525d08
Scala Steward <scala_steward@virtuslab.com>
2022-07-27 18:24:14 +0000

    Update protobuf-java to 3.21.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val protobufVersion = "3.20.1"
+  lazy val protobufVersion = "3.21.4"
   lazy val protobuf =
     "com.google.protobuf" % "protobuf-java-util" % protobufVersion
   lazy val guava = "com.google.guava" % "guava" % "31.1-jre"
