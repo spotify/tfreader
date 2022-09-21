9f1791f15b578d91cf120142e305de7a3ddd5e76
regadas <oss@regadas.email>
2022-09-21 13:04:57 +0100

    Update circe deps to v0.14.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   lazy val catsCore = "org.typelevel" %% "cats-core" % "2.8.0"
   lazy val scallop = "org.rogach" %% "scallop" % "4.1.0"
   lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "2.5.1"
-  lazy val circeCore = "io.circe" %% "circe-core" % "0.14.2"
+  lazy val circeCore = "io.circe" %% "circe-core" % "0.14.3"
   lazy val munit = "org.scalameta" %% "munit" % "0.7.29"
-  lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.2"
+  lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.3"
 }
