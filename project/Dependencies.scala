e7223b551d2e9abdcc631839151f9374b0a04ef3
Scala Steward <scala_steward@virtuslab.com>
2023-08-15 21:16:53 +0000

    Update cats-core to 2.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val protobuf =
     "com.google.protobuf" % "protobuf-java-util" % protobufVersion
   lazy val guava = "com.google.guava" % "guava" % "32.1.2-jre"
-  lazy val catsCore = "org.typelevel" %% "cats-core" % "2.9.0"
+  lazy val catsCore = "org.typelevel" %% "cats-core" % "2.10.0"
   lazy val fs2Io = "co.fs2" %% "fs2-io" % "3.8.0"
   lazy val scallop = "org.rogach" %% "scallop" % "5.0.0"
   lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "2.26.0"
