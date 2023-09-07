c5fd9a912fbc1fa698221aeb18856c845efdc061
Scala Steward <scala_steward@virtuslab.com>
2023-09-07 08:03:07 +0000

    Update circe-core, circe-parser to 0.14.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   lazy val fs2Io = "co.fs2" %% "fs2-io" % "3.8.0"
   lazy val scallop = "org.rogach" %% "scallop" % "5.0.0"
   lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "2.26.1"
-  lazy val circeCore = "io.circe" %% "circe-core" % "0.14.5"
+  lazy val circeCore = "io.circe" %% "circe-core" % "0.14.6"
   lazy val munit = "org.scalameta" %% "munit" % "0.7.29"
-  lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.5"
+  lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.6"
 }
