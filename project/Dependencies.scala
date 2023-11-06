bbab011dbd025db0ee1464a81b348d3f1e76c0f1
Scala Steward <scala_steward@virtuslab.com>
2023-11-06 21:27:15 +0000

    Update scallop to 5.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   lazy val guava = "com.google.guava" % "guava" % "32.1.3-jre"
   lazy val catsCore = "org.typelevel" %% "cats-core" % "2.10.0"
   lazy val fs2Io = "co.fs2" %% "fs2-io" % "3.9.2"
-  lazy val scallop = "org.rogach" %% "scallop" % "5.0.0"
+  lazy val scallop = "org.rogach" %% "scallop" % "5.0.1"
   lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "2.29.0"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.14.6"
   lazy val munit = "org.scalameta" %% "munit" % "0.7.29"
