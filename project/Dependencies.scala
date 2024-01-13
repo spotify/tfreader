f0f43f823677f0c8a0c46c99a8f9051be6b7a505
Scala Steward <scala_steward@virtuslab.com>
2024-01-13 11:14:11 +0000

    Update fs2-io to 3.9.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "com.google.protobuf" % "protobuf-java-util" % protobufVersion
   lazy val guava = "com.google.guava" % "guava" % "32.1.3-jre"
   lazy val catsCore = "org.typelevel" %% "cats-core" % "2.10.0"
-  lazy val fs2Io = "co.fs2" %% "fs2-io" % "3.9.2"
+  lazy val fs2Io = "co.fs2" %% "fs2-io" % "3.9.3"
   lazy val scallop = "org.rogach" %% "scallop" % "5.0.1"
   lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "2.29.0"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.14.6"
