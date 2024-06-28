74d8b35adacb321fe4fdd89d8f7d4038ca78c9d5
Scala Steward <scala_steward@virtuslab.com>
2024-06-28 21:37:27 +0000

    Update google-cloud-storage to 2.40.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   lazy val catsCore = "org.typelevel" %% "cats-core" % "2.12.0"
   lazy val fs2Io = "co.fs2" %% "fs2-io" % "3.10.2"
   lazy val scallop = "org.rogach" %% "scallop" % "5.1.0"
-  lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "2.40.0"
+  lazy val gcs = "com.google.cloud" % "google-cloud-storage" % "2.40.1"
   lazy val circeCore = "io.circe" %% "circe-core" % "0.14.7"
   lazy val munit = "org.scalameta" %% "munit" % "1.0.0"
   lazy val circeParser = "io.circe" %% "circe-parser" % "0.14.7"
